4874f3372ab3d252625a90debe022dec5bda9194
Esteban Arocha <e.arocha@ukko.mx>
2020-05-23 23:09:02 -0500

    Sign in in other page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import history from './helpers/history'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { withAuthentication } from './components/Session';
 import * as ROUTES from './constants/routes';
+import { AuthUserContext } from './components/Session';
 
 //Components
 import Sidebar from './components/Sidebar'
@@ -23,18 +24,22 @@ import './App.css'
 
 const App = () => (
   <Router>
-    <Switch>
-          <div className="App">
-             <div id="wrapper">
-                <Sidebar path={history.location.pathname}/>
-                <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-                <Route exact path={ROUTES.APPROVED_MAILS} component={ApprovedMailsIndex} />
-                <Route exact path={ROUTES.APPROVED_MAILS_NEW} component={ApprovedMailsNew} />
-                <Route exact path={ROUTES.APPROVED_MAILS_EDIT} component={ApprovedMailsEdit} />
-                <Route exact path={ROUTES.APPROVED_MAILS_SHOW} component={ApprovedMailsShow} />
-            </div>
-          </div>
-    </Switch>
+    <AuthUserContext.Consumer>
+      {authUser => (
+         authUser ?  <Switch>
+         <div className="App">
+            <div id="wrapper">
+               <Sidebar path={history.location.pathname}/>
+               <Route exact path={ROUTES.APPROVED_MAILS} component={ApprovedMailsIndex} />
+               <Route exact path={ROUTES.APPROVED_MAILS_NEW} component={ApprovedMailsNew} />
+               <Route exact path={ROUTES.APPROVED_MAILS_EDIT} component={ApprovedMailsEdit} />
+               <Route exact path={ROUTES.APPROVED_MAILS_SHOW} component={ApprovedMailsShow} />
+           </div>
+         </div>
+   </Switch> : <SignInPage />
+      )}
+    </AuthUserContext.Consumer>
+    
   </Router>
 );
  
